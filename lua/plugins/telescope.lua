61e53cec9226394854991994105bf4a7e452364c
Cosmeak <guillaume.fine@cosmeak.dev>
2023-12-15 17:06:08 +0100

    feat: remove vendor and mode_modules from telescope research

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,14 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      require("telescope").setup({
+        defaults = { 
+          file_ignore_patterns = {
+            "node_modules",
+            "vendor",
+          }
+        }
+      })
     end
   },
 }
